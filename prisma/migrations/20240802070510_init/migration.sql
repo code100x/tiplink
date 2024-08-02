4fbad603eafeaaae4cbc012e2512b909fae77357
Arpit Blagan <arpitblagan@Arpits-MacBook-Air.local>
2024-08-02 12:43:51 +0530

    added dark mode

---
+++ prisma/migrations/20240802070510_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "publicAddress" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,21 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export function Hero() {
   return (
-    <div className="flex flex-col min-h-[100dvh] bg-zinc-800">
+    <div className="flex flex-col min-h-[100dvh]">
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
-                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-white">
+                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none ">
                     Secure Your Crypto with Our Wallet Generator
                   </h1>
-                  <p className="max-w-[600px] text-muted-foreground md:text-xl text-white">
-                    Generate a secure, private crypto wallet in minutes with our easy-to-use tool. Powered by Google
-                    Auth for seamless, secure access.
+                  <p className="max-w-[600px] text-muted-foreground md:text-xl">
+                    Generate a secure, private crypto wallet in minutes with our
+                    easy-to-use tool. Powered by Google Auth for seamless,
+                    secure access.
                   </p>
                 </div>
               </div>
@@ -27,14 +28,22 @@ export function Hero() {
           &copy; 2024 Crypto Wallet Generator. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link href="#" className="text-xs hover:underline underline-offset-4 text-white" prefetch={false}>
+          <Link
+            href="#"
+            className="text-xs hover:underline underline-offset-4 text-white"
+            prefetch={false}
+          >
             Terms of Service
           </Link>
-          <Link href="#" className="text-xs hover:underline underline-offset-4 text-white" prefetch={false}>
+          <Link
+            href="#"
+            className="text-xs hover:underline underline-offset-4 text-white"
+            prefetch={false}
+          >
             Privacy
           </Link>
         </nav>
       </footer>
     </div>
-  )
+  );
 }
