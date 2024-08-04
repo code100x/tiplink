6bf3cfe406ce0e6db2fed6240001d32317545eae
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-04 21:51:31 +0530

    Enhance user interface to differentiate from TipLinks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Appbar from "@/components/Appbar/Appbar";
 import { Footer } from "@/components/Appbar/Footer";
 import Home from "@/components/Home/Home";
 
+
 export default function Page() {
   return (
     <>
