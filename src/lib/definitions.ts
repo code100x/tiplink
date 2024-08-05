3a5ae23cb81f0665ea635bc7a201bda868a78432
Riya267 <riyacec05@gmail.com>
2024-08-05 11:25:01 +0530

    rebased

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,26 +1,26 @@
 export interface CustomSectionItemsProps {
-    title: string;
-    description: string;
-    bgBlue: boolean;
-    iconSvg: any;
-    iconTitle: string;
-    imgDirLeft: boolean;
-    image: any;
+  title: string
+  description: string
+  bgBlue: boolean
+  iconSvg: any
+  iconTitle: string
+  imgDirLeft: boolean
+  image: any
 }
 
 export interface Card {
-    id: number;
-    title: string;
-    iconImag: any;
-    img: any;
-};
+  id: number
+  title: string
+  iconImag: any
+  img: any
+}
 
 export interface CustomSectionsProps {
-    title: string;
-    description: string;
-    iconSvg: any;
-    iconTitle: string;
-    image: any;
-    bgBlue: boolean;
-    imgDirLeft: boolean;
-};
+  title: string
+  description: string
+  iconSvg: any
+  iconTitle: string
+  image: any
+  bgBlue: boolean
+  imgDirLeft: boolean
+}
