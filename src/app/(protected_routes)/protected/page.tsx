e7eb4d8028bbe45ff85f359b4333293e7bf7255c
aloksharma10 <aloksharma10969@gmail.com>
2024-08-08 02:54:04 +0530

    [BUGFIX]: Prevent unauthorised to go to Protected routes #63

---
+++ src/app/(protected_routes)/protected/page.tsx
@@ -0,0 +1,8 @@
+export default function ProtectedPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Protect Page</h1>
+            <p className="text-lg text-gray-700">You are accessing protected page.</p>
+        </div>
+    )
+}
