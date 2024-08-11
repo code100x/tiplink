55d078621fc1bdca77d9a41028c49d506ea5c050
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-12 00:42:59 +0530

    added wallet adapter

---
+++ src/components/AppWalletProvider.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React, { useMemo } from 'react'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { clusterApiUrl } from '@solana/web3.js'
+// import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css')
+
+// imports here
+
+export default function AppWalletProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const wallets = useMemo(
+    () => [
+      // manually add any legacy wallet adapters here
+      // new UnsafeBurnerWalletAdapter(),
+    ],
+    [network],
+  )
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
