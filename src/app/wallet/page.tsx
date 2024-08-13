fc1ab834f8bee67c335e2e836d0d1a11644040b3
Ameer jafar <ameerjafar123@gmail.com>
2024-08-13 20:20:28 +0530

    align the code properly

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,30 +1,30 @@
-"use client";
+'use client'
 
-import { useState } from "react";
-import LeftSideBar from "./LeftSideBar";
-import RightSideBar from "./RightSideBar";
-import TopBar from "./TopBar";
-import WalletDetail from "./WalletDetail";
+import { useState } from 'react'
+import LeftSideBar from './LeftSideBar'
+import RightSideBar from './RightSideBar'
+import TopBar from './TopBar'
+import WalletDetail from './WalletDetail'
 
 const Wallet = () => {
-
-
   return (
     <div>
-        <div><TopBar /></div>
-        <div className="flex justify-between pt-10">
-        <div className = 'hidden sm:block'>
+      <div>
+        <TopBar />
+      </div>
+      <div className="flex justify-between pt-10">
+        <div className="hidden sm:block">
           <LeftSideBar />
         </div>
         <div>
           <WalletDetail />
         </div>
-        <div className = 'pr-15 hidden md:block'>
+        <div className="pr-15 hidden md:block">
           <RightSideBar />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Wallet;
+export default Wallet
