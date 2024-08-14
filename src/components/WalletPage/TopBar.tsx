d4b40773f5985a8018e1c568c00c6d0add400ce6
Ameer jafar <ameerjafar123@gmail.com>
2024-08-14 19:56:23 +0530

    remove the conflicts

--- src/components/WalletPage/TopBar.tsx
+++ src/components/WalletPage/TopBar.tsx
@@ -1,6 +1,6 @@
 import Logo from '../icons/Logo'
-<<<<<<< HEAD
-import Sidebar from '../../app/wallet/ui/Sidebar'
+import Sidebar from './Sidebar'
+
 import { useSession } from 'next-auth/react'
 import {
   DropdownMenu,
@@ -8,10 +8,7 @@ import {
 } from '@radix-ui/react-dropdown-menu'
 import { UserRound } from 'lucide-react'
 import UserImage from '@/components/Appbar/UserImage'
-=======
-import Sidebar from './Sidebar'
 
->>>>>>> 677ae1afcee64357db28ed2389df1578458b91bc
 const TopBar = () => {
   const { data } = useSession()
   return (
@@ -19,7 +16,7 @@ const TopBar = () => {
       <div className="block sm:hidden">
         <Sidebar />
       </div>
-      <div className="sm:block border h-6 w-6 sm:h-10 sm:w-10 rounded-lg inline-flex justify-center items-center">
+      <div className="hidden sm:block border h-6 w-6 sm:h-10 sm:w-10 rounded-lg inline-flex justify-center items-center">
         <Logo className="h-4 w-4 sm:h-8 sm:w-8" fill="#000000" />
       </div>
       <div className="pl-[5%] sm:pl-[30%] pt-3 sm:pt-0 ">
