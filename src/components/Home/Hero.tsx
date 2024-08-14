06a021a7b364d5097d42d9ab7d623c206f0e7998
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-08-14 18:47:48 +0530

    modified

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -50,12 +50,26 @@ const router = useRouter();
             </span>
           </Button>
           :
-          <Button className="pl-2 py-6 text-sm md:text-base" onClick={()=>{
+          <Button className="pl-3 py-6 text-sm bg-black md:text-base" onClick={()=>{
             router.push("/wallet");
           }}>
-          <div className="px-3 py-2 rounded-lg bg-black text-white">
-             Go to wallet
+                <span className="flex items-center gap-2">
+          <div className="px-3 py-2 rounded-lg bg-white text-black">
+          <svg
+            className="size-5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="128"
+            height="128"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M4 3a3 3 0 0 0-3 3v13a3 3 0 0 0 3 3h13a3 3 0 0 0 3-3v-1.77c.63-.57 1-1.38 1-2.23v-5c0-.85-.37-1.66-1-2.23V6a3 3 0 0 0-3-3zm0 1h13a2 2 0 0 1 2 2v1.17c-.32-.11-.66-.17-1-.17h-6a3 3 0 0 0-3 3v5a3 3 0 0 0 3 3h6c.34 0 .68-.06 1-.17V19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2m8 4h6a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-6a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2m2.5 2a2.5 2.5 0 0 0-2.5 2.5a2.5 2.5 0 0 0 2.5 2.5a2.5 2.5 0 0 0 2.5-2.5a2.5 2.5 0 0 0-2.5-2.5m0 1a1.5 1.5 0 0 1 1.5 1.5a1.5 1.5 0 0 1-1.5 1.5a1.5 1.5 0 0 1-1.5-1.5a1.5 1.5 0 0 1 1.5-1.5"
+            />
+          </svg>
           </div>
+             Go to wallet
+          </span>
         </Button>
         }
         </div>
