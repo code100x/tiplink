446536bc991aab9a7c00b9ba25741f7ac19dcbf3
Saurav1509 <saurav.sayana@gmail.com>
2024-08-31 18:53:25 +0530

    #146: Fix the Wallet page user Icon to be same as the Home page user icon dropdown

--- src/components/WalletPage/TopBar.tsx
+++ src/components/WalletPage/TopBar.tsx
@@ -17,6 +17,14 @@ import UserImage from '@/components/Appbar/UserImage'
 import { useState } from 'react'
 import LeftSideBar from './LeftSideBar'
 
+const dropDownData = [
+  {
+    name: 'Profile',
+    icon: <UserRound size={15} />,
+    href: '/profile',
+  },
+]
+
 const TopBar = () => {
   const router = useRouter();
 
@@ -49,8 +57,8 @@ const TopBar = () => {
        {open?<div className="bg-[#1c1c1cd3] bg-blur-md transition-y mt-9 duration-300 z-10  w-[95%] h-[88%] fixed left-1/2  m-0 rounded-md overflow-hidden origin-top -translate-x-1/2"><LeftSideBar/></div>:<>
        </>}
       </div>
-      <div className="hidden sm:block border h-6 w-6 sm:h-10 sm:w-10 rounded-lg justify-center items-center">
-        <Logo className="h-4 w-4 sm:h-8 sm:w-8" fill="#000000" />
+      <div className="border h-10 w-10 rounded-lg inline-flex justify-center zitems-center">
+        <Logo className="h-8 w-8" fill="#000000" />
       </div>
       <div className="hidden sm:block sm:w-1/2 md:w-[50%] ">
         <input
@@ -61,7 +69,7 @@ const TopBar = () => {
       <div className="">
         {data && data?.user && (
           <DropdownMenu>
-            <DropdownMenuTrigger className=" flex items-center   justify-center  transition">
+            <DropdownMenuTrigger className="w-[3rem] flex items-center p-[0.2rem]  justify-center h-[2rem] transition">
               {!data?.user.image ? (
                 <div className="p-1 border-2 rounded-md">
                   <UserRound />
@@ -70,6 +78,54 @@ const TopBar = () => {
                 <UserImage image={data?.user.image} />
               )}
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent className=" dark:shadow-[#030712] translate-y-8 scale-110 -translate-x-10 shadow-lg bg-white">
+              <DropdownMenuLabel className="flex gap-4 items-center">
+                <div className="!w-[2rem] flex items-center p-[0.2rem]  justify-center !h-[2rem]">
+                  {!data?.user.image ? (
+                    <div className="p-1 border-2 rounded-full border-[#1a1a1a]">
+                      <UserRound />
+                    </div>
+                  ) : (
+                    <UserImage image={data?.user.image} />
+                  )}
+                </div>
+
+                <div className="flex flex-col">
+                  <span className="max-w-[200px]">{data?.user?.name}</span>
+                  <span className="text-[0.8rem] max-w-[200px] text-gray-400">
+                    {data?.user?.email}
+                  </span>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+
+              {dropDownData.map((item, index) => {
+                return (
+                  <DropdownMenuItem
+                    className="flex gap-2 cursor-pointer text-black/70 hover:text-black transition"
+                    onClick={() => router.push('/profile')}
+                    key={index}
+                  >
+                    <span>{item.icon}</span>
+                    <span>{item.name}</span>
+                  </DropdownMenuItem>
+                )
+              })}
+              <DropdownMenuSeparator />
+              {data?.user && (
+                <DropdownMenuItem
+                  onClick={async () => {
+                    await signOut()
+                    router.push('/')
+                  }}
+                  className="flex gap-2 cursor-pointer text-black/70 hover:text-black transition"
+                >
+                  <LogOut size={15} />
+                  Logout
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
           </DropdownMenu>
         )}
       </div>
