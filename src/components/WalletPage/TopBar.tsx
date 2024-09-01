968eb205f32efa8d8c44afd56f93a19ccd0d0967
arunprajapati-arch <arunprajapati.mail@gmail.com>
2024-09-01 07:45:36 +0530

    dropdown fix

--- src/components/WalletPage/TopBar.tsx
+++ src/components/WalletPage/TopBar.tsx
@@ -17,6 +17,14 @@ import UserImage from '@/components/Appbar/UserImage'
 import { useState } from 'react'
 import LeftSideBar from './LeftSideBar'
 
+const dropDownData = [
+  {
+    name: 'Profile',
+    icon: <UserRound size={15} />,
+    href: '/profile',
+  },
+]
+
 const TopBar = () => {
   const router = useRouter();
 
@@ -59,19 +67,69 @@ const TopBar = () => {
         />
       </div>
       <div className="">
-        {data && data?.user && (
+        {data && data?.user  ? (
+          
           <DropdownMenu>
             <DropdownMenuTrigger className=" w-[3rem] flex items-center p-[0.2rem]  justify-center h-[2rem] transition">
+              
               {!data?.user.image ? (
-                <div className="p-1 border-2 rounded-md">
-                  <UserRound />
+                <div className="  p-1 border-2 rounded-md">
+                  <UserRound  />
                 </div>
               ) : (
                 <UserImage image={data?.user.image} />
               )}
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent className=" dark:shadow-[#030712] translate-y-8 scale-110 -translate-x-10 shadow-lg bg-white">
+            <DropdownMenuLabel className="flex gap-4 items-center">
+                    <div className="!w-[2rem] flex items-center p-[0.2rem]  justify-center !h-[2rem]">
+                      {!data?.user.image ? (
+                        <div className="p-1 border-2 rounded-full border-[#1a1a1a]">
+                          <UserRound />
+                        </div>
+                      ) : (
+                        <UserImage image={data?.user.image } />
+                      )}
+                    </div>
+
+                    <div className="flex flex-col">
+                      <span className="max-w-[200px]">{data?.user?.name}</span>
+                      <span className="text-[0.8rem] max-w-[200px] text-gray-400">
+                        {data?.user?.email}
+                      </span>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+
+                  {dropDownData.map((item, index) => {
+                    return (
+                      <DropdownMenuItem
+                        className="flex gap-2 cursor-pointer text-black/70 hover:text-black transition"
+                        onClick={() => router.push('/profile')}
+                        key={index}
+                      >
+                        <span>{item.icon}</span>
+                        <span>{item.name}</span>
+                      </DropdownMenuItem>
+                    )
+                  })}
+                  <DropdownMenuSeparator />
+                  {data?.user && (
+                    <DropdownMenuItem
+                      onClick={async () => {
+                        await signOut()
+                        router.push('/')
+                      }}
+                      className="flex gap-2 cursor-pointer text-black/70 hover:text-black transition"
+                    >
+                      <LogOut size={15} />
+                      Logout
+                    </DropdownMenuItem>
+                  )}
+            </DropdownMenuContent>
           </DropdownMenu>
-        )}
+        ): <div className='w-[3rem] flex items-center p-[0.2rem]  justify-center h-[2rem] rounded-md' ><UserRound size={30} /> </div> }
       </div>
     </div>
   )
