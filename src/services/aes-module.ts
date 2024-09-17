40b43cb424dee5899bc60d00032ce4e4dcaa4b95
rahulbarmann <itsrahulbarman1@gmail.com>
2024-09-18 02:32:10 +0530

    feat: encrypt one share using aes-256 and store it in db refactor: update function names of aes, aws and gcp encrypt and decrypt functions

--- src/services/aes-module.ts
+++ src/services/aes-module.ts
@@ -3,7 +3,7 @@ import crypto from 'crypto'
 const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY
 const IV_LENGTH = 16
 
-export function encrypt(apiKey: any) {
+export function aesEncrypt(plainText: any) {
   if (!ENCRYPTION_KEY) {
     throw new Error('Encryption key is not set')
   }
@@ -16,18 +16,18 @@ export function encrypt(apiKey: any) {
     iv,
   )
 
-  let encrypted = cipher.update(apiKey, 'utf8', 'hex')
+  let encrypted = cipher.update(plainText, 'utf8', 'hex')
   encrypted += cipher.final('hex')
 
   return iv.toString('hex') + ':' + encrypted
 }
 
-export function decrypt(encryptedApiKey: any) {
+export function aesDecrypt(encryptedData: any) {
   if (!ENCRYPTION_KEY) {
     throw new Error('Encryption key is not set')
   }
 
-  const textParts = encryptedApiKey.split(':')
+  const textParts = encryptedData.split(':')
   const iv = Buffer.from(textParts.shift(), 'hex')
   const encryptedText = Buffer.from(textParts.join(':'), 'hex')
 
