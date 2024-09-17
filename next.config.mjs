02a3afc27d6c534bcd854a78680c1d26c6e64ab7
rahulbarmann <itsrahulbarman1@gmail.com>
2024-09-17 19:58:05 +0530

    feat: add aws-kms-module and gcp-kms-module

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,11 @@ const nextConfig = {
   },
   env: {
     ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
+    AWS_CMK_ARN: process.env.AWS_CMK_ARN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    LOCATION_ID: process.env.LOCATION_ID,
+    KEY_RING_ID: process.env.KEY_RING_ID,
+    KEY_ID: process.env.KEY_ID,
   },
 }
 
